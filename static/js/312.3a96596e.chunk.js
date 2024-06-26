e4e87bcaeae72ce5306c024b84df800ab73c5cb1
ishmaael93 <ishmaael93@gmail.com>
2024-06-26 21:23:19 +0300

    Updates

---
+++ static/js/312.3a96596e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktapok=self.webpackChunktapok||[]).push([[312],{312:(e,n,s)=>{s.r(n),s.d(n,{default:()=>l});var a=s(43),r=s(117),t=s(579);const c=()=>(0,t.jsx)(r.es,{children:(0,t.jsx)(r.b7,{name:"App",children:(0,t.jsx)(r.uA,{name:"Loader",children:(0,t.jsx)(r.KT,{children:(0,t.jsx)(d,{})})})})});function d(){return(0,t.jsx)(a.Fragment,{children:"Loading..."})}const l=()=>(0,t.jsx)(r.m8,{palette:(0,t.jsx)(c,{})})}}]);
+//# sourceMappingURL=312.3a96596e.chunk.js.map
